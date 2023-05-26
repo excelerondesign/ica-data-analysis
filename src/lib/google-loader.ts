e6d9c767f0d6d5298f5592741cce445d40ad95af
Connor Wilson <connor.wilson@excelerondesigns.com>
2023-05-26 16:38:53 -0400

    Adds temp fix for map not rendering properly

--- src/lib/google-loader.ts
+++ src/lib/google-loader.ts
@@ -1,7 +1,8 @@
 import { Loader } from '@googlemaps/js-api-loader';
 
+/** Below is a bandaid solution for netlify not being able to get env variables the same way Astro does locally */
 const loader = new Loader({
-    apiKey: import.meta.env.PUBLIC_GOOGLE_MAPS,
+    apiKey: import.meta.env.PUBLIC_GOOGLE_MAPS ?? 'AIzaSyA35IqWX-zVrvJyG5UPX3BeRtO1IudPGKw',
     version: 'weekly'
 });
 
