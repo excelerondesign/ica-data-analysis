c53032653ec2c1089604d83423ac910d457248ef
Connor Wilson <connor.wilson@excelerondesigns.com>
2023-06-01 12:29:00 -0400

    Adds initial InfoWindow support
    fixes an issue with `.setVisible()` and `reset()` not
    actually setting the visibility of when called.
    
    Now the functions more explicitly call `.setVisible()`
    
    With the updating of how filtering worked, this was *super* necesssary

--- src/lib/Markers.ts
+++ src/lib/Markers.ts
@@ -1,6 +1,9 @@
 import { on } from "./Events";
 import { library } from "./google-loader";
+
 const { Marker } = await library.maps.importLibrary('marker');
+const { InfoWindow } = await library.maps.importLibrary('maps');
+
 export type MarkerIndiv = typeof library.maps.Marker;
 export type MarkerGroup = MarkerIndiv[];
 export type MarkerMap = typeof library.maps.Map;
@@ -8,18 +11,32 @@ export type MarkerBounds = typeof library.maps.LatLngBounds;
 export class Markers {
     _markers: MarkerGroup;
     map: MarkerMap;
+    _info: typeof InfoWindow;
     constructor(markers: MarkerGroup, map: MarkerMap, bounds:MarkerBounds) {
+        this._info = new InfoWindow({
+            content: '',
+            disableAutoPan: true
+        });
+
         this._markers = markers.map(m => {
             const position = m['Patient LatLng'];
 
             bounds.extend(position);
 
-            return new Marker({
+            const marker = new Marker({
                 position,
                 data: m,
                 map
             });
-        })
+
+            marker.addListener('click', () => {
+                this._info.setContent(m['Patient LatLng'].name);
+                this._info.open(map, marker);
+            });
+
+            return marker;
+        });
+
         this.map = map;
         on('markers:reset', () => this.reset());
     }
@@ -28,8 +45,8 @@ export class Markers {
         return this._markers.filter(m => m.getVisible());
     }
 
-    setVisible(filter: (value: MarkerIndiv, index: number, array: MarkerGroup) => void) {
-        this._markers.forEach(filter);
+    setVisible(filter: (value: MarkerIndiv, index?: number, array?: MarkerGroup) => void) {
+        this._markers.forEach(marker => marker.setVisible(filter(marker)));
     }
 
     filter(filter: (value: MarkerIndiv, index: number, array: MarkerGroup) => boolean) {
@@ -37,7 +54,7 @@ export class Markers {
     }
 
     reset() {
-        this.setVisible(Boolean);
+        this.setVisible((m:MarkerIndiv) => m.setVisible(true));
     }
 
     all() {
